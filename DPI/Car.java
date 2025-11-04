f750c5d151cfddd1ef0ecdabd1b30502cef435c5
JulianDwiP <juliandwi74@gmail.com>
2025-11-04 19:35:46 +0700

    Add File Car.jav DPI

---
+++ DPI/Car.java
@@ -0,0 +1,10 @@
+class Car { 
+    private final Engine engine; 
+    public Car(Engine engine) { 
+        this.engine = engine; 
+    } 
+    
+    void start() { 
+        engine.start(); 
+    } 
+}
